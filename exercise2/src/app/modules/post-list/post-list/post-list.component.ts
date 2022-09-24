8772c20405433a2b8dedcdf701f1d0b76fadc608
R4vage <jmarc@applaudostudios.dev>
2022-09-24 14:24:10 -0300

    Last touches

--- exercise2/src/app/modules/post-list/post-list/post-list.component.ts
+++ exercise2/src/app/modules/post-list/post-list/post-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PostList implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
--- exercise2/src/app/modules/post-list/post/post.component.ts
+++ exercise2/src/app/modules/post-list/post/post.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class Post implements OnInit {
 
+  
+  @Input() postText:string = "";
+  @Input() postDate:string = "";
+  @Input() postImgUrl:string = "";
   constructor() { }
 
   ngOnInit(): void {
